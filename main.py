942e4d0dbd7c1334948612fc4540c69a5f37ed57
Dmitiy <dimapermyakov55@gmail.com>
2022-11-14 15:27:24 +0300

    

--- main.py
+++ main.py
@@ -4,5 +4,6 @@ from longpoll_bot import LongPollBot
 from nlu_longpoll_bot import NLULongPollBot
 
 if __name__ == '__main__':
+
     long_poll_bot = LongPollBot()
     long_poll_bot.run_long_poll()
--- my_test.py
+++ my_test.py
@@ -1,11 +1,25 @@
 # Copyright © 2022 mightyK1ngRichard <dimapermyakov55@gmail.com>
 import re
+import asyncio
+
+
+async def fun1():
+    print('START')
+    await asyncio.sleep(2)
+    print('END')
+
+
+async def fun2():
+    print('START NEW')
+    await asyncio.sleep(2)
+    print('END NEW')
 
 
 def main():
-    pattern = re.compile(r'(^(((н|h)+(е|e)+т*)|(n+o+)|(н|h(о|o)+у|y))*\s*,*\.*\s*((т+ы+)+|(y|у+o|о+u+)+|u+))[\.,]*$')
-    if pattern.match('нет, ты.  '):
-        print('EE')
+    loop = asyncio.get_event_loop()
+    task1 = loop.create_task(fun1())
+    task2 = loop.create_task(fun2())
+    loop.run_until_complete(asyncio.wait([task1, task2]))
 
 
 if __name__ == '__main__':
