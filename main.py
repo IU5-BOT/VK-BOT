10514ce21ee5f1d6b74b363a35c79db975f3b32f
Dmitiy <dimapermyakov55@gmail.com>
2023-03-01 12:41:08 +0300

    

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from longpoll_bot import LongPollBot
 
 if __name__ == '__main__':
-    long_poll_bot = LongPollBot()
-    long_poll_bot.run_long_poll()
+    try:
+        long_poll_bot = LongPollBot()
+        long_poll_bot.run_long_poll()
+    except:
+        print('ЛОЛ')
